306dac1f182909764d09183b72cea4fcb4c489c0
Gutemberg Medeiros <gutenbergdevm@hotmail.com>
2022-04-03 16:21:19 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Produto from "./Produto";
+
+// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
+// Defina o produto clicado como uma preferência do usuário no localStorage
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
+
 
 function App() {
+  const [produto, setProduto] = React.useState(null);
+
+  React.useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto');
+
+    if(produtoLocal !== null) setProduto(produtoLocal);
+  }, [])
+
+  React.useEffect(() => {
+    if (produto !== null) window.localStorage.setItem('produto', produto);
+  }, [produto])
+
+  function handleClick(event) {
+    setProduto(event.target.innerText);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Preferência: {produto}</h1>
+      <button onClick={handleClick} >notebook</button>
+      <button onClick={handleClick} >smartphone</button>
+
+      <Produto produto={produto} />
     </div>
   );
 }
